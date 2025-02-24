935e85db3f0157da7744ddb28eb3603becb21cf0
adrieljss <adrielsiahaya@gmail.com>
2025-02-24 23:21:32 +0800

    feat: main preview with sidebar

--- src/pages/preview/index.tsx
+++ src/pages/preview/index.tsx
@@ -1,9 +1,17 @@
-import PreviewFloating from '../../components/PreviewFloating'
+import { useState } from 'react'
+import CollapsibleSidebar from '../../components/CollapsibleSidebar'
+import MainView from '../../components/MainView'
 
 export default function Preview() {
+  const sidebarState = useState(false)
+
   return (
     <>
-      <PreviewFloating />
+      {/* <PreviewFloating /> */}
+      <div className='flex bg-primary-100'>
+        <CollapsibleSidebar sidebarState={sidebarState} />
+        <MainView isSidebarOpen={sidebarState[0]} />
+      </div>
     </>
   )
 }
