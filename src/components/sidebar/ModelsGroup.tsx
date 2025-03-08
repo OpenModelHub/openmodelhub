c22d4d9b6f4e5ce31edd078c062161759154524b
adrieljss <adrielsiahaya@gmail.com>
2025-03-08 23:49:35 +0800

    feat: improve model fetching

--- src/components/sidebar/ModelsGroup.tsx
+++ src/components/sidebar/ModelsGroup.tsx
@@ -12,21 +12,32 @@ const ModelsGroup = () => {
   const { models, setModels, setMessages } = React.useContext(GlobalContext)
   const [fetchLoading, setLoading] = React.useState(false)
   const [filterValue, setFilter] = React.useState('')
+  const { pushNotification } = React.useContext(NotificationContext)
 
-  const refreshModels = () => {
+  const retrieveModels = (isInitialLoad: boolean = false) => {
+    // prevent spam
     setLoading(true)
     fetchModels()
       .then((models) => {
+        const initModelMessages: Record<string, ChatDisplayMessage[]> = {}
         const initModelInfo: Record<string, Model> = {}
         models.models.forEach((model) => {
           initModelInfo[model.name] = model
+          if (isInitialLoad) initModelMessages[model.name] = []
         })
 
+        if (isInitialLoad) setMessages(initModelMessages)
         setModels(initModelInfo)
-        pushNotification('success', 'Successfully refreshed models.')
+        pushNotification('success', 'Successfully retrieved models.')
       })
       .catch((e) => {
-        pushNotification('error', e)
+        const err: string = e.toString()
+        if (err.includes('error sending')) {
+          pushNotification(
+            'error',
+            `Ollama server might be inactive, unable to fetch Ollama models.\nError: ${e}`
+          )
+        } else pushNotification('error', e)
       })
       .finally(() => setLoading(false))
   }
@@ -35,31 +46,9 @@ const ModelsGroup = () => {
     .filter((model) => model.name.startsWith(filterValue))
     .sort((model1, model2) => model1.name.localeCompare(model2.name))
 
-  const { pushNotification } = React.useContext(NotificationContext)
-
-  const fetchInitialModels = () => {
-    fetchModels()
-      .then((models) => {
-        const initModelMessages: Record<string, ChatDisplayMessage[]> = {}
-        const initModelInfo: Record<string, Model> = {}
-        models.models.forEach((model) => {
-          initModelMessages[model.name] = []
-          initModelInfo[model.name] = model
-        })
-
-        setMessages(initModelMessages)
-        setModels(initModelInfo)
-
-        pushNotification('success', 'Successfully fetched models.')
-      })
-      .catch((e) => {
-        pushNotification('error', e)
-      })
-  }
-
   React.useEffect(() => {
     // TODO: add loading screen
-    fetchInitialModels()
+    retrieveModels(true)
   }, [])
 
   return (
@@ -67,18 +56,19 @@ const ModelsGroup = () => {
       <Typography variant='caption' className='mb-1 text-primary-800'>
         {filtered.length} models found
       </Typography>
-      <div className='flex mb-2'>
+      <div className='flex mb-2 relative'>
         <input
           type='text'
-          className='grow resize-none w-full outline-none placeholder-primary-900 bg-primary-300 px-4 py-2 rounded-l-md text-sm'
+          className='grow resize-none w-full outline-none placeholder-primary-900 bg-primary-300 px-4 py-2 rounded-md text-sm'
           value={filterValue}
           onChange={(e) => setFilter(e.target.value)}
           placeholder='Search Models...'
         />
         <Button
           variant='icon'
-          onClick={refreshModels}
-          className='rounded-l-none bg-primary-300'
+          onClick={() => retrieveModels()}
+          disabled={fetchLoading}
+          className='absolute right-1 top-0.5 p-2 bg-primary-300 disabled:cursor-not-allowed disabled:opacity-40'
         >
           <ArrowPathIcon className={`w-4 ${fetchLoading && 'animate-spin'}`} />
         </Button>
@@ -87,7 +77,7 @@ const ModelsGroup = () => {
       {filtered.length > 0 ? (
         filtered.map((model, i) => <ModelButton model={model.name} key={i} />)
       ) : (
-        <div className='select-none w-full mt-10 items-center justify-center grid space-y-1'>
+        <div className='select-none w-full mt-20 items-center justify-center grid space-y-1'>
           <FolderOpenIcon
             className='w-full items-center justify-center text-gray-400'
             width={50}
